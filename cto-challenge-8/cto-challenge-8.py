0504d1ca3f9731c66a88a4a7e550e1a7beed0226
denis-wane <denis-wane@users.noreply.github.com>
2017-07-31 16:25:33 -0400

    Final Version

--- cto-challenge-8/cto-challenge-8.py
+++ cto-challenge-8/cto-challenge-8.py
@@ -12,14 +12,10 @@ import sys
 
 import matplotlib 
 import pandas
-from pandas.plotting import scatter_matrix
-from sklearn import (metrics, cross_validation, linear_model, preprocessing)
 from sklearn import model_selection
 import sklearn
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.ensemble import (RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier)
 from sklearn.ensemble import RandomForestClassifier as RF
-from sklearn.feature_extraction import DictVectorizer as DV
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
@@ -30,16 +26,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import LabelEncoder
-from sklearn.svm import SVC
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 import csv as csv
-from keras.layers import Dense
-from keras.models import Sequential
-from keras.utils import np_utils
-from keras.wrappers.scikit_learn import KerasClassifier
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -48,14 +38,14 @@ import seaborn as sns
 train_path = ".\contest-train.csv"
 out_path = ".\contest-test.csv"
 
-train_base = pd.read_csv(train_path,header=0, encoding="utf-8")
-out_base = pd.read_csv(out_path,header=0,encoding="utf-8")
+train_base = pd.read_csv(train_path, header=0, index_col=0, encoding="utf-8")
+full_base = pd.read_csv(out_path, header=0, index_col = 0, encoding="utf-8")
 
-cols_to_drop = ['UID', 'ID', 'Course Name', 'Course URL / Code', 'Certification URL','Consolidated Course Name', 'Assigned To', 'Request Status', 'Start Date', 'End Date', 'Start Mo/Yr', 'End Mo/Yr', 'Start FY', 'End FY', 'Individual Travel Hours', 'Rqst Tot Labor Hrs', 'Airfare', 'Hotel', 'Per Diem', 'Other', 'Estimated Individual Travel', 'Misc Expenses', 'Catering', 'Facility Rental', 'Direct Other Expenses', 'Describe Other Expenses', 'Direct Expense Impact', 'Rqst NPR Alloc', 'Rqst NPR OH', 'Cancel No Response', 'Created', 'Retroactive Start Date', 'Duplicates', 'Reporting Status']
+cols_to_drop = ['ID', 'Course Name', 'Course URL / Code', 'Certification URL','Consolidated Course Name', 'Assigned To', 'Request Status', 'Start Date', 'End Date', 'Start Mo/Yr', 'End Mo/Yr', 'Start FY', 'End FY', 'Individual Travel Hours', 'Rqst Tot Labor Hrs', 'Airfare', 'Hotel', 'Per Diem', 'Other', 'Estimated Individual Travel', 'Misc Expenses', 'Catering', 'Facility Rental', 'Direct Other Expenses', 'Describe Other Expenses', 'Direct Expense Impact', 'Rqst NPR Alloc', 'Rqst NPR OH', 'Cancel No Response', 'Created', 'Retroactive Start Date', 'Duplicates', 'Reporting Status']
 categorical_columns = ['Training Source', 'Home Office/Metro Area', 'Organization Number', 'Organization', 'Capability', 'Function 2', 'Career Level', 'Function', 'Function Name', 'Title','Training Type', 'Training Provider', 'Training Delivery Type', 'Training Location', 'Vendor Name', 'Conference Name', 'Course or Event Name', 'Certification Type', 'Certification Name', 'Is there a course with this certification?', 'Activity', 'Support Group', 'Business Justification', 'What % of the conference is business development?', 'Travel Required']
 
 train_base = train_base.drop(cols_to_drop, axis=1)
-out_base = out_base.drop(cols_to_drop, axis=1)
+out_base = full_base.drop(cols_to_drop, axis=1)
 
 full_set = pd.concat([train_base,out_base], axis=0)
 x_full_preprocessed = pd.get_dummies(data = full_set, columns = categorical_columns, drop_first = True)
@@ -74,8 +64,8 @@ for cat in categories:
     category_map [cat] = i
     i  = i+1
 
-#split the training portion
-train, test =  train_test_split(x_train_fin, test_size = .1)
+#split the training portion - setting very low for final pass, use .2 for reasonable accuracy test
+train, test =  train_test_split(x_train_fin, test_size = .02)
 
 #separate Category
 x_train = train.drop(['Category'], axis = 1)
@@ -126,13 +116,14 @@ predictions = LR.predict(x_test)
 print (accuracy_score(y_test_preprocessed.values.ravel(), predictions))
 
 predictions_test = LR.predict(x_out_fin)
-print(predictions_test)
-print (predictions_test.shape)
+# print (category_map)
+# print(predictions_test)
+# print (predictions_test.shape)
 
-i = 0
+i = 1
 for row in predictions_test:
     cat_val = list(category_map.keys())[list(category_map.values()).index(row)]
-    out_base.ix[i,'Category'] = cat_val
+    full_base.ix[i,'Category'] = cat_val
     i = i+1
     
-out_base.to_csv('results.csv')    
+full_base.to_csv('results.csv')    
